b4f0aa9590415cc9efebab0a38f61a6ba9298f4b
Aden Linday <lindsaya542@gmail.com>
2025-12-18 15:51:35 +1030

    chore: template

---
+++ www/nuxt.config.ts
@@ -0,0 +1,42 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  extends: ['..'],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        language: 'en-US',
+      },
+      {
+        code: 'fr',
+        name: 'Français',
+        language: 'fr-FR',
+      },
+      {
+        code: 'km',
+        name: 'ភាសាខ្មែរ',
+        language: 'km-KH',
+      },
+      {
+        code: 'it',
+        name: 'Italiano',
+        language: 'it-IT',
+      },
+      {
+        code: 'zhcn',
+        name: '简体中文',
+        language: 'zh-CN',
+      },
+    ],
+  },
+  ogImage: {
+    fonts: [
+      'Geist:400',
+      'Geist:700',
+    ],
+  },
+  compatibilityDate: '2025-05-13',
+});
